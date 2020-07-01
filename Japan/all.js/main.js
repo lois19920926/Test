ed10c4d664ed2dea8eae2ccfb47144abd05b4a4e
Lois <lois19920926@gmail.com>
2020-07-01 21:53:50 +0800

    no message

---
+++ Japan/all.js/main.js
@@ -0,0 +1,14 @@
+
+
+var uuid ='';
+var token = '';
+var apiPath = '';
+
+var btn = document.querySelector('#btn');
+
+btn.addEventListener("click",getData);
+
+function getData(){
+    console.log('按鈕');
+    var api = ``;
+}
