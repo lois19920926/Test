5a7d147bc12491ad3d06a3c3e8a9168bb441509f
Lois <lois19920926@gmail.com>
2020-07-02 19:06:12 +0800

    forEach

--- all.js/main.js
+++ all.js/main.js
@@ -2,10 +2,8 @@ var uuid ='64349c0e-ef4d-436d-b317-a419a2161160';
 var token = 'SO51PiJyqrKrdilUsD9QUYLMKSr8vU7qvwagMGMr95MssanSxK5qIUdvcDMd';
 var apiPath = 'https://course-ec-api.hexschool.io/';
 
-
 //驗證
 axios.defaults.headers['Authorization'] = `Bearer ${token}`;
-
 
 var data;
 var obj = {
@@ -16,7 +14,7 @@ var obj = {
         console.log(api);
         axios.get(api)
             .then (function(res) {
-                console.log(res);
+//                console.log(res);
                 console.log(res.data.data);
                 vm.data = res.data.data;
                 vm.render();
@@ -28,22 +26,27 @@ var obj = {
         },
         render(){  //obj裡的函式
             console.log(this);
-            document.querySelector('.mainpic')
-                .innerHTML=
-            `<div class="list" id="${this.data[0].id}">
-                    <div class="pic">
-                        <img src="${this.data[0].imageUrl[0]}" alt="">
-                    </div>
-                    <div class="content">
-                        <ul>
-                            <li class="productName">${this.data[0].title}</li>
-                            <li class="productDescription">
-                            </li>
-                            <li class="productPrice">NT: ${this.data[0].price}</li>
-                            <li class="productAdd">加入購物車</li>
-                        </ul>
-                    </div>
-                    `
+            var str = '';
+            this.data.forEach((item,index)=>{
+                str +=
+                `
+                <li>
+                <div class="list" id="${this.data[index].id}">
+                <div class="pic">
+                    <img src="${this.data[index].imageUrl[0]}" alt="">
+                </div>
+                <div class="content">
+                    <ul>
+                        <li class="productName">${this.data[index].title}</li>
+                        <li class="productDescription">${this.data[index].content}</li>
+                        <li class="productPrice">NT: ${this.data[index].price}</li>
+                        <li class="productAdd">加入購物車</li>
+                    </ul>
+                </div>
+                </li>
+                `;
+            })
+            document.querySelector('.mainList').innerHTML = str;        
         }
     }
 
